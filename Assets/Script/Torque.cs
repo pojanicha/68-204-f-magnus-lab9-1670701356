66f7baf364ea0ae1d171ba64d764a6d9ebb9aff0
pojanicha <pojanicha.sora@bumail.net>
2026-03-12 15:03:35 +0700

    Football

---
+++ Assets/Script/Torque.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Torque : MonoBehaviour
+{
+    public float torque;
+    Rigidbody rb;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Keyboard.current.dKey.isPressed)
+        {
+            rb.AddTorque(new Vector3(0, torque, 0));
+        }
+
+    }
+}
